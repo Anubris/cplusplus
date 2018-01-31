0473d2def6e09787c14debad8e8e88178ac99e26
Anubris <oscar.merigou@gmail.com>
2018-01-31 14:31:25 +0100

    exo1 terminé

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,10 +1,16 @@
-//  Copyright (c) 2017 Antoine Tran Tan
+//  Copyright (c) 2017 Antoine Tran 
+//  Copyright (c) 2018 Oscar Poulot
 //
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
 
-
+double exo1(double rayon)
+{
+	double volume;
+	volume = 4.00 / 3.00 * 3.14 * rayon * rayon * rayon;
+	return volume;
+}
 
 #endif // MY_HEADER_HPP
 
